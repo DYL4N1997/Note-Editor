20a2a1b8c78822d135b7907b017c8be25247d024
Dylan <user@users-MacBook-Pro-2.local>
2022-05-23 00:04:14 +0100

    updating client service worker js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,1 +1,7 @@
 // TODO: Create a service worker that caches static assets:
+const { warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
