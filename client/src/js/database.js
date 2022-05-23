257ae33b7fbdb2af11ec8337d6ce75ce51bbd52a
Dylan <user@users-MacBook-Pro-2.local>
2022-05-23 13:39:23 +0100

    updating databse js logic

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -2,9 +2,17 @@
 import { openDB } from 'idb';
 
 // TODO: Complete the initDb() function below:
-const initdb = async () => {
-
-};
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
 
 
 // TODO: Complete the postDb() function below:
